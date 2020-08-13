94a565f8f7999e2cf81f5d260eb6145a4d035df1
himanshuhy <himanshu.hy95@gmail.com>
2020-08-13 18:19:54 +0530

    backup

--- src/database.js
+++ src/database.js
@@ -8,25 +8,3 @@ mongoose.connect(mongoURI, {useNewUrlParser:true, useUnifiedTopology:true})
 var db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-
-let Schema = mongoose.Schema
-
-let person = new Schema({
-    name: String,
-    birthday: Date,
-    updated: {type: Date, default: new Date()},
-    age: {type: Number, required:true, min:18, max: 65},
-    contact: [],
-    nested: {stuff: {wow: String}},
-    foreignKey: Schema.Types.ObjectId
-})
-//Model becomes a collection in mongoDB
-let PersonModel = mongoose.model('PersonModel', person)
-
-let person1 = new PersonModel({name:'Himanshu', age: 24});
-
-console.log("Himanshu");
-// console.log(person1.save())
-
-// console.log(person1.birthday);
---
+++ src/models/author.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var AuthorSchema = new Schema(
+  {
+    first_name: {type: String, required: true, maxlength: 100},
+    family_name: {type: String, required: true, maxlength: 100},
+    date_of_birth: {type: Date},
+    date_of_death: {type: Date},
+  }
+);
+
+AuthorSchema.virtual('lifespan').get(function() {
+    return (this.date_of_death.getYear() - this.date_of_birth.getYear()).toString()
+});
+
+AuthorSchema.virtual('url').get(function() {
+    return '/catalog/author/' + this._id;
+});
+
+module.exports = mongoose.model('Author', AuthorSchema);
